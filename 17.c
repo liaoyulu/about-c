da897b8f90c8048d975c23632b37a4c5872ef014
liaoyulu <1257816990@qq.com>
2019-10-21 23:39:02 +0800

    review

--- 17.c
+++ 17.c
@@ -7,12 +7,15 @@ int main()
 	do
 	{
 		for(i=2;i<=10000;i++)
-		{	isprime=1;
+		{	
+			isprime=1;
 			for(j=2;j<i;j++)
 			{
 				if(i%j==0)
+				{
 					isprime=0;
 					break;
+				}
 			}
 			if(isprime){p[num]=i;num++;}
 		}
